0645f942a6c6eacb1eb852d70d8091775296e241
Igor Kuritsin onive <mail@onive.ru>
2019-01-10 23:18:56 +0800

    test: change separator, for running tests on different platforms

--- spec/src/main.spec.ts
+++ spec/src/main.spec.ts
@@ -1,5 +1,5 @@
 import { promises } from "fs";
-import { join, resolve } from "path";
+import { join, resolve, sep } from "path";
 import compile from "../helpers/compile";
 import { baseUrl, outDir as actualPath } from "../helpers/config";
 
@@ -9,10 +9,14 @@ const expectPath = resolve(baseUrl, "./expect");
 compile(join(compilePath, "./*.ts"));
 
 async function assertFilesEqual(fileName: string) {
-  const [actualRaw, expectRaw] = await Promise.all([
+  let [actualRaw, expectRaw] = await Promise.all([
     promises.readFile(join(actualPath, fileName), { encoding: "utf8" }),
     promises.readFile(join(expectPath, fileName), { encoding: "utf8" })
   ]);
+
+  if (sep === "\\") {
+    actualRaw = actualRaw.replace(/\\\\/g, "/");
+  }
 
   expect(actualRaw).toEqual(expectRaw);
 }
