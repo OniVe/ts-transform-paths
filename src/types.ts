5e4c3b07a17722ad2d01059b1d167c806c97186b
Igor Kuritsin onive <mail@onive.ru>
2019-03-02 20:20:13 +0800

    refactor: Removed unnecessary comments

--- src/types.ts
+++ src/types.ts
@@ -73,7 +73,7 @@ export class PathAliasResolver {
         relativePath = "." + path.sep + relativePath;
       }
 
-      return relativePath.replace(REGEXP_ALL_BACKSLASH, "/"); //;
+      return relativePath.replace(REGEXP_ALL_BACKSLASH, "/");
     } else {
       return requestedModule;
     }
