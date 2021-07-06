1e349e84f3d6edee053b3608d90102b2c90d7ddc
Martin Donath <squidfunk@users.noreply.github.com>
2021-07-06 10:53:17 +0200

    fix: `ImportTypeNode` not mapped in `*.d.ts` files (#27)
    * fix: ImportTypeNode not mapped in d.ts files
    
    * docs: update example for import types

--- src/transformer.ts
+++ src/transformer.ts
@@ -55,6 +55,14 @@ export function transformerFactory<T extends TransformerNode>(
 
       /**
        * e.g.
+       * - type Foo = import('path').Foo;
+       */
+      if (ts.isImportTypeNode(node)) {
+        return ts.visitEachChild(node, pathReplacer, context);
+      }
+
+      /**
+       * e.g.
        * - import * as x from 'path';
        * - import { x } from 'path';
        */
